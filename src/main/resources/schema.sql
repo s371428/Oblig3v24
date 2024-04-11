c62d790637488542aed844bf32968a406461bd81
Mewan Andre Mohammad <s371428@oslomet.no>
2024-04-11 10:03:55 +0200

    Endret på Billett tabellen i schema sql filen

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,10 +2,10 @@ CREATE TABLE Billett
 (
   id INTEGER AUTO_INCREMENT NOT NULL,
   film VARCHAR(255) NOT NULL,
-  antall VARCHAR(255) NOT NULL,
+  antall INTEGER NOT NULL,
   fornavn VARCHAR(255) NOT NULL,
   etternavn VARCHAR(255) NOT NULL,
-  telefonnr VARCHAR(255) NOT NULL,
+  telefonnr CHAR(8) NOT NULL,
   epost VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 );
